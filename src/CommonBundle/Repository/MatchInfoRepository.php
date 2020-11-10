d5d28899d00251522a2e56a9c4d0e7b92512ab80
Marquis <511680713@qq.com>
2020-11-10 22:24:55 +0800

    [update]

--- src/CommonBundle/Repository/MatchInfoRepository.php
+++ src/CommonBundle/Repository/MatchInfoRepository.php
@@ -19,4 +19,48 @@ class MatchInfoRepository extends \Doctrine\ORM\EntityRepository
     {
         return $this->createQueryBuilder('q');
     }
+
+    /**
+     * @param $cId
+     * @param $title
+     * @param $isOnline
+     * @param $type
+     * @param $currentSortOrder
+     * @return QueryBuilder|mixed
+     */
+    public function searchByCategory($cId, $isOnline, $type, $title, $currentSortOrder)
+    {
+        $result = $this->createQueryBuilder('q')
+            ->leftJoin('q.MatchCategory', 'm')
+            ->where('q.title LIKE :title');
+
+        if ($cId != 0) {
+            $result = $result
+                ->andWhere('m.id = :cId')
+                ->setParameter('cId', $cId);
+        }
+
+        if($isOnline != "null"){
+            $result = $result
+                ->andWhere('m.isOnline = :isOnline')
+                ->setParameter('isOnline', $isOnline);
+        }
+
+        if($type != "null"){
+            $result = $result
+                ->andWhere('m.type = :type')
+                ->setParameter('type', $isOnline);
+        }
+
+        if ($currentSortOrder != "null") {
+            $result = $result
+                ->orderBy('q.currentCost', $currentSortOrder);
+        }
+
+        $result = $result
+            ->setParameter('title', "%{$title}%")
+            ->getQuery()
+            ->getResult();
+        return $result;
+    }
 }
